8bf6e0480fc43ae83a6e96be29cbe91d35235896
ErlynDeleon <152839946+ErlynDeleon@users.noreply.github.com>
2024-10-15 10:45:50 +0800

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,96 @@
 import Image from "next/image";
+import {
+   Card,
+   CardContent,
+   CardDescription,
+   CardHeader,
+   CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   return (
+      // Root div
+      <div className="flex h-screen">
+         {/* Parent div for the left side */}
+         <div className="w-2/3 h-full flex items-center justify-center">
+            <div className="relative w-full h-full flex flex-col p-4">
+               {/* Dark overlay for the image */}
+               <div className="absolute inset-0 bg-black opacity-15 z-10"></div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+               <div className="relative z-20 flex items-center justify-between text-3xl font-bold text-white mb-auto">
+                  <div className="flex items-center">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="mr-2 h-6 w-6"
+                     >
+                        <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+                     </svg>
+                     arcanum
+                  </div>
+
+                  {/* Added logo on the opposite side */}
+                  <div className="relative w-16 h-16">
+                     <Image
+                        src="/neu-logo.png"
+                        alt="New Era University Logo"
+                        layout="fill" 
+                        objectFit="contain"
+                     />
+                  </div>
+               </div>
+
+               <Image
+                  src="/hero.jpg"
+                  alt="New Era University"
+                  className="rounded-md object-cover w-full h-full"
+                  layout="fill"
+               />
+               <div className="relative z-20 mt-auto text-white">
+                  <blockquote className="space-y-2">
+                     <p className="text-lg">
+                        A resource hub that empowers students and researchers
+                        from New Era University and beyond, <br /> fostering a
+                        community where knowledge is accessible and shared
+                        across different institutes.
+                     </p>
+                     <footer className="text-sm font-bold">
+                        Developed by corruptedFile
+                     </footer>
+                  </blockquote>
+               </div>
+            </div>
+         </div>
+
+         {/* Parent div for the right side i.e. the form */}
+         <div className="w-1/3 h-full flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-semibold">arcanum</h1>
+            <p className="text-base text-muted-foreground mb-4">
+               Powered by New Era Main Library
+            </p>
+            <Card className="w-full max-w-md px-2 shadow-md rounded-lg">
+               <CardHeader>
+                  <CardTitle className="text-xl">Welcome!</CardTitle>
+                  <CardDescription className="text-base">
+                     Are you from within New Era University?
+                  </CardDescription>
+               </CardHeader>
+               <CardContent className="space-y-4">
+                  <Button variant="default" className="w-full">
+                     Sign in with my institutional email
+                  </Button>
+                  <Button variant="outline" className="w-full">
+                     Continue as guest
+                  </Button>
+               </CardContent>
+            </Card>
+         </div>
+      </div>
+   );
 }
