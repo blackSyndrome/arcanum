37850df62bfd422229600abd9ce25d104bf34a2c
romnoelp <petracorta.rmnl@gmail.com>
2024-10-18 15:36:55 +0800

    Finished the dashboard navbar

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -15,7 +15,6 @@ export async function GET(request: Request) {
    if (code) {
       const supabase = createClient();
       const { error } = await supabase.auth.exchangeCodeForSession(code);
-      console.log(error);
 
       if (!error) {
          const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
--- src/components/authentication/LoginPageLeftSide.tsx
+++ src/components/authentication/LoginPageLeftSide.tsx
@@ -7,31 +7,28 @@ const LoginPageLeftSide = () => {
             {/* Dark overlay for the image */}
             <div className="absolute inset-0 bg-black opacity-25 z-10"></div>
 
-            <div className="relative z-20 flex items-center justify-between text-xl md:text-3xl font-bold text-white mb-auto">
-               <div className="flex items-center">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     fill="none"
-                     stroke="currentColor"
-                     strokeWidth="2"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                     className="mr-2 h-5 md:h-6 w-5 md:w-6"
-                  >
-                     <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-                  </svg>
-                  arcanum
+            <div className="relative z-20 flex items-center justify-center text-xl md:text-3xl font-bold text-white mb-auto">
+               <div className="flex items-center justify-center">
+                  <div className="relative h-12 md:h-12 w-12 md:w-12 mr-2">
+                     <Image
+                        src="/arcanum-logo.svg"
+                        alt="Arcanum logo"
+                        fill
+                        style={{ objectFit: "contain" }}
+                     />
+                  </div>
+                  <span className="ml-2">arcanum</span>
                </div>
 
-               {/* Added logo on the opposite side */}
-               <div className="relative w-10 md:w-16 h-10 md:h-16">
-                  <Image
-                     src="/neu-logo.png"
-                     alt="New Era University Logo"
-                     fill
-                     style={{ objectFit: "contain" }}
-                  />
+               <div className="ml-auto">
+                  <div className="relative w-12 md:w-16 h-12 md:h-16">
+                     <Image
+                        src="/neu-logo.png"
+                        alt="New Era University Logo"
+                        fill
+                        style={{ objectFit: "contain" }}
+                     />
+                  </div>
                </div>
             </div>
 
@@ -42,6 +39,7 @@ const LoginPageLeftSide = () => {
                fill
                style={{ objectFit: "cover" }}
             />
+
             <div className="relative z-20 mt-auto text-white">
                <blockquote className="space-y-2">
                   <p className="text-sm md:text-lg">
