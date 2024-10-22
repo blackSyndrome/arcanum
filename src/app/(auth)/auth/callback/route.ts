e8ad5a1f8dcac6e4bf84dd773a8bb5c872026ff3
romnoelp <petracorta.rmnl@gmail.com>
2024-10-22 22:41:35 +0800

    Finished the front end side

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
    const { searchParams, origin } = new URL(request.url);
    const code = searchParams.get("code");
    // if "next" is in param, use it as the redirect URL
-   const next = searchParams.get("next") ?? "/dashboard";
+   const next = searchParams.get("next") ?? "/role";
 
    console.log("origin", origin);
    console.log("next", next);
